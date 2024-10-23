07ba9e23275c20b632823dca4c743e2de684216f
Mahsa Behmanesh <mahsa.behmanesh74@gmail.com>
2024-10-23 08:22:24 +0330

    remove "rewrites()" from  next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*", // This matches any request to /api/*
-        destination: `${process.env.NEXT_PUBLIC_PARSE_ADDRESS}/:path*`, // Proxy to the external API
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
